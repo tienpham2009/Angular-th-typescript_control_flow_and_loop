759dc041eecf876a50938a8a7e1fbcc94e26398a
tienpham2009 <tiendx2009@gmail.com>
2021-07-12 14:49:30 +0700

    typescript-control-flow-and-loop version 2

--- src/main.js
+++ src/main.js
@@ -19,4 +19,26 @@ for (let i = 2; count < 30; i++) {
     sum += i;
     count++;
 }
-console.log(sum);
+function isPrime(number) {
+    let isPrime = true;
+    if (number < 2) {
+        isPrime = false;
+    }
+    else if (number > 2) {
+        for (let i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
+}
+let array = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+let total = 0;
+for (let number of array) {
+    if (isPrime(number)) {
+        total += number;
+    }
+}
+console.log("Tổng các số nguyên tố trong mảng trên là: " + total);
--- src/main.ts
+++ src/main.ts
@@ -24,5 +24,29 @@ for (let i = 2; count < 30 ; i++) {
     count++;
 }
 
-console.log(sum);
+function isPrime(number:number){
+    let isPrime: boolean = true;
+    if(number < 2 ){
+        isPrime = false;
+    }else if(number > 2){
+        for( let i = 2 ; i < Math.sqrt(number); i++){
+            if(number % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    return isPrime;
+
+}
+
+let array = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+let total: number = 0;
+for (let number of array) {
+    if (isPrime(number)) {
+        total += number;
+    }
+}
+console.log("Tổng các số nguyên tố trong mảng trên là: " + total);
 
